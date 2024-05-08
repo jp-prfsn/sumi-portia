e68196a94c44f08bbd139257814aa6b1f1c5ca53
patchware <jpb3rz@gmail.com>
2024-05-08 21:04:07 +0100

    Adds waves and trees

---
+++ Assets/Tree.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tree : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        Invoke("PlayRandomAnimation", Random.Range(5, 10));
+        
+    }
+
+    void PlayRandomAnimation(){
+        anim.SetTrigger("Shrug");
+
+        Invoke("PlayRandomAnimation", Random.Range(5, 10));
+    }
+}
---
+++ Assets/TreeGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeGenerator : MonoBehaviour
+{
+
+    public Vector2 range;
+    public GameObject tree;
+
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // split the range into a 2d grid and generate a tree in every cell
+
+        for (float x = -range.x; x < range.x; x++)
+        {
+            for (float y = -range.y; y < range.y; y++)
+            {
+                Vector3 randomOffset = new Vector3(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f), 0);
+                Instantiate(tree, transform.position + new Vector3(x, y, 0) + randomOffset, Quaternion.identity, transform);
+            }
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireCube(transform.position, new Vector3(range.x * 2, range.y * 2, 0.1f));
+    }
+
+    
+}
